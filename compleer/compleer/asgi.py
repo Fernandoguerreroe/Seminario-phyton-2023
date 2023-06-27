17ce1975d37a2508f2352549bdf7a6d6889edb0e
Fernandoguerreroe <fernandoguerreroe86@gmail.com>
2023-06-27 13:36:15 -0500

    Creando proyecto de django

--- compleer/compleer/asgi.py
+++ compleer/compleer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for compleer project.
+ASGI config for CompLeer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compleer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CompLeer.settings')
 
 application = get_asgi_application()
--- compleer/compleer/urls.py
+++ compleer/compleer/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for compleer project.
+URL configuration for CompLeer project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
